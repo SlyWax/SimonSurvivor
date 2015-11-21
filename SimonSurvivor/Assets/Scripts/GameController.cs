cf6eb636a0d6e54833f9248b9b885c6815b15f78
Gérard Mendes <gerard.mendes@gmail.com>
2015-11-21 17:42:11 +0100

    Color pipes

--- SimonSurvivor/Assets/Scripts/GameController.cs
+++ SimonSurvivor/Assets/Scripts/GameController.cs
@@ -10,6 +10,9 @@ using AssemblyCSharp;
 public class GameController : MonoBehaviour {
 	public Text sequenceHelper;
 	public TextMesh scoreKeeper;
+	public float intervalBetweenSequences = 1f;
+	public float pipeLightingTime = 1f;
+	public float pipeIntervalTime = 1f;
 	public float trapDoorClosingTime = 1f;
 	public float trapDoorOpeningTime = 1f;
 	public float trapDoorWaitingTime = 2f;
@@ -20,10 +23,16 @@ public class GameController : MonoBehaviour {
 	private GameObject greenZone;
 	private GameObject redZone;
 	private GameObject yellowZone;
+	private GameObject pipeA;
+	private GameObject pipeB;
+	private GameObject pipeC;
+	private GameObject pipeD;
 	private Camera mainCamera;
 
-    private BallSequenceGenerator generator;
+    private BallSequenceGenerator ballGenerator;
+	private PipeSequenceGenerator pipeGenerator;
 	private int score;
+	private IList<Pipe> remainingPipes;
 	private IList<BallColor> remainingSequence;
 
 	// Use this for initialization
@@ -32,6 +41,10 @@ public class GameController : MonoBehaviour {
 		greenZone = GameObject.Find("Green Zone");
 		redZone = GameObject.Find("Red Zone");
 		yellowZone = GameObject.Find("Yellow Zone");
+		pipeA = GameObject.Find("Pipe A");
+		pipeB = GameObject.Find("Pipe B");
+		pipeC = GameObject.Find("Pipe C");
+		pipeD = GameObject.Find("Pipe D");
         playerController = GameObject.Find("Player").GetComponent<PlayerController>();
 		mainCamera = Camera.main;
 		ResetSequence();
@@ -87,6 +100,21 @@ public class GameController : MonoBehaviour {
 		return isInSafeZone;
 	}
 
+	private Color ColorFor(BallColor color) {
+		switch (color) {
+		case BallColor.Blue:
+			return Color.blue;
+		case BallColor.Red:
+			return Color.red;
+		case BallColor.Yellow:
+			return Color.yellow;
+		case BallColor.Green:
+			return Color.green;
+		default:
+			return Color.white;
+		} 
+	}
+
 	private GameObject ZoneFor(BallColor color) {
 		switch (color) {
 		case BallColor.Blue:
@@ -102,21 +130,70 @@ public class GameController : MonoBehaviour {
 		} 
 	}
 
+	private GameObject PipeFor(Pipe pipe) {
+		switch (pipe) {
+		case Pipe.A:
+			return pipeA;
+		case Pipe.B:
+			return pipeB;
+		case Pipe.C:
+			return pipeC;
+		case Pipe.D:
+			return pipeD;
+		default:
+			return null;
+		} 
+	}
+
 	private void ResetSequence() {
 		score = 0;
-		generator = new BallSequenceGenerator().addNewBall();
-		PrepareTrapDoorSequence();
+		pipeGenerator = new PipeSequenceGenerator().addNewPipe();
+		ballGenerator = new BallSequenceGenerator().addNewBall();
+		PlaySequenceToRemember();
+		//PrepareTrapDoorSequence();
 	}
 	
 	private void IncrementSequence() {
-		score++;
-		generator = generator.addNewBall();
-		PrepareTrapDoorSequence();
+		score++;		
+		pipeGenerator = pipeGenerator.addNewPipe();
+		ballGenerator = ballGenerator.addNewBall();
+		PlaySequenceToRemember();
+		//PrepareTrapDoorSequence();
+	}
+
+	private void PlaySequenceToRemember() {
+		remainingPipes = pipeGenerator.sequence.ToList<Pipe>();
+		remainingSequence = ballGenerator.sequence.ToList<BallColor>();
+		InvokeRepeating("UpdatePipes", intervalBetweenSequences, pipeLightingTime + pipeIntervalTime);
+	}
+
+	private void UpdatePipes() {
+		if (remainingPipes.Any ()) {
+			LightPipeWithColor(remainingPipes.First<Pipe>(), remainingSequence.First<BallColor> ());
+			remainingPipes = remainingPipes.Skip (1).ToList<Pipe> ();
+			remainingSequence = remainingSequence.Skip (1).ToList<BallColor> ();
+			Invoke ("TurnOffPipes", pipeLightingTime);
+		} else {
+			CancelInvoke();
+			PrepareTrapDoorSequence();
+		}
+	}
+
+	private void LightPipeWithColor(Pipe pipe, BallColor ballColor) {
+		PipeFor(pipe).GetComponent<Renderer>().material.color = ColorFor(ballColor);
+	}
+
+	private void TurnOffPipes() {
+		var pipeTypes = Enum.GetValues(typeof(Pipe)).Cast<Pipe>();
+		var allPipes = pipeTypes.Select(p => PipeFor(p));
+		foreach (GameObject pipe in allPipes) {
+			pipe.GetComponent<Renderer>().material.color = Color.white;
+		}
 	}
 
 	private void UpdateSequenceHelper() {
-		sequenceHelper.text = "" + generator.sequence.Select(c => c.ToString("F"))
-			                                         .Aggregate((c, n) => c + "-" + n);
+		sequenceHelper.text = "" + ballGenerator.sequence.Select(c => c.ToString("F"))
+			                                             .Aggregate((c, n) => c + "-" + n);
 	}
 
 	
@@ -126,8 +203,8 @@ public class GameController : MonoBehaviour {
 
 	private void PrepareTrapDoorSequence() {
 		UpdateSequenceHelper ();
-		UpdateScoreKeeper ();
-		remainingSequence = generator.sequence.ToList<BallColor> ();
+		UpdateScoreKeeper();
+		remainingSequence = ballGenerator.sequence.ToList<BallColor> ();
 		InvokeRepeating("UpdateTrapDoors", sequenceCountDown, trapDoorOpeningTime + trapDoorClosingTime + trapDoorWaitingTime);
 	}
 
