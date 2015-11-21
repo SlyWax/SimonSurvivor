e2d4fcdebe0f28317d2e88f1c4a82261a726b47e
Gérard Mendes <gerard.mendes@gmail.com>
2015-11-21 03:46:12 +0100

    added colored tiles scene

---
+++ SimonSurvivor/Assets/Scripts/ZoneController.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ZoneController : MonoBehaviour {
+	public bool isOpening = false;
+	public float openingSpeed = 5.0f;
+	public float closingSpeed = 10.0f;
+	public Vector3 transformOrigin;
+	public Vector3 transformDestination;
+
+	// Use this for initialization
+	void Start () {
+		transformOrigin = transform.position;
+		transformDestination = new Vector3 (transform.position.x * 2, transform.position.y, transform.position.z * 2);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		float step;
+		if (isOpening) {
+			step = openingSpeed * Time.deltaTime;
+			transform.position = Vector3.MoveTowards (transform.position, transformDestination, step);
+		} else {
+			step = closingSpeed * Time.deltaTime;
+			transform.position = Vector3.MoveTowards (transform.position, transformOrigin, step);
+		}
+	}
+
+	public ZoneController changeState() {
+		isOpening = !isOpening;
+		return this;
+	}
+}
